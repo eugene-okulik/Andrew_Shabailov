5b89ee52b44439e566a2d606064b4b310930e5ef
AndrewShabailov <andrewshabailov@gmail.com>
2026-04-01 14:43:20 +0300

    completed Task2

---
+++ homework/andrew_shabailov/lesson5/task_2.py
@@ -0,0 +1,15 @@
+result_1 = 'результат операции: 42'
+result_2 = 'результат операции: 514'
+result_3 = 'результат работы программы: 9'
+
+num1 = result_1[-2::]
+_num1 = int(num1) + 10
+print(_num1)
+
+num2 = result_2[-3::]
+_num2 = int(num2) + 10
+print(_num2)
+
+num3 = result_3[-1]
+_num3 = int(num3) + 10
+print(_num3)
