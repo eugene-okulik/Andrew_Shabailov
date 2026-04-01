86999e3d56296f2d47999d60d4bb7425bf1c9188
AndrewShabailov <andrewshabailov@gmail.com>
2026-04-01 13:44:35 +0300

    completed Task1

---
+++ homework/andrew_shabailov/lesson5/task_1.py
@@ -0,0 +1,9 @@
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+
+name = person[0]
+last_name = person[1]
+city = person[2]
+phone = person[3]
+country = person[4]
+
+print(f'Name - {name}, Last Name - {last_name}, City - {city}, Phone - {phone}, Country - {country}')
