4cf7e24fb81cedd200a2edff2fb151e0216db3b7
AndrewShabailov <andrewshabailov@gmail.com>
2026-04-02 10:09:45 +0300

    task1 - changed way to parse data

--- homework/andrew_shabailov/lesson5/task_1.py
+++ homework/andrew_shabailov/lesson5/task_1.py
@@ -1,9 +1,3 @@
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
-
-name = person[0]
-last_name = person[1]
-city = person[2]
-phone = person[3]
-country = person[4]
-
-print(f'Name - {name}, Last Name - {last_name}, City - {city}, Phone - {phone}, Country - {country}')
+name, last_name, city, phone, country = person
+print(f'name - {name}, last_name - {last_name}, city - {city}, phone - {phone}, country - {country}')
