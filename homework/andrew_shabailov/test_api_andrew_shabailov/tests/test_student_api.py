f5a04744587362b8f3ed89f8ddc3b51518d19b61
AndrewShabailov <andrewshabailov@gmail.com>
2026-05-21 19:08:07 +0300

    added endpoint fixtures, deleted extra code and class initialization

--- homework/andrew_shabailov/test_api_andrew_shabailov/tests/test_student_api.py
+++ homework/andrew_shabailov/test_api_andrew_shabailov/tests/test_student_api.py
@@ -5,6 +5,7 @@ from endpoints.update_student import UpdateStudent
 from endpoints.delete_student import DeleteStudent
 from endpoints.patch_student import PatchStudent
 from endpoints.change_student import ChangeStudent
+from endpoints.common_base import Base
 
 
 data_d = {
@@ -64,9 +65,8 @@ def test_get_info_by_student_id(
         student_id,
         read_student_endpoint,
 ):
-    read_student = ReadStudent()
-    read_student.read_student(student_id)
-    read_student.read_status_code_is_200()
+    read_student_endpoint.read_student(student_id)
+    read_student_endpoint.read_status_code_is_200()
 
 
 @pytest.mark.positive
@@ -74,21 +74,21 @@ def test_update_student(
         start_end_testing,
         before_after_testing,
         student_id,
+        update_student_endpoint
 ):
-    update_student = UpdateStudent()
-    update_student.update_student(student_id, payload=data_upd)
-    update_student.update_status_code_is_200()
+    update_student_endpoint.update_student(student_id, payload=data_upd)
+    update_student_endpoint.update_status_code_is_200()
 
 
 @pytest.mark.positive
 def test_patch_student(
         start_end_testing,
         before_after_testing,
-        student_id
+        student_id,
+        patch_student_endpoint,
 ):
-    patch_student = PatchStudent()
-    patch_student.patch_student(student_id, payload=patch_data)
-    patch_student.patch_status_code_is_200()
+    patch_student_endpoint.patch_student(student_id, payload=patch_data)
+    patch_student_endpoint.patch_status_code_is_200()
 
 
 @pytest.mark.positive
@@ -98,9 +98,8 @@ def test_delete_student(
         student_id_to_delete,
         delete_student_endpoint
 ):
-    delete_student = DeleteStudent()
-    delete_student.delete_student(student_id_to_delete)
-    delete_student.delete_status_code_is_200()
+    delete_student_endpoint.delete_student(student_id_to_delete)
+    delete_student_endpoint.delete_status_code_is_200()
 
 
 @pytest.mark.positive
@@ -110,27 +109,24 @@ def test_add_student(
         before_after_testing,
         payload,
         create_student_endpoint,
-        read_student_endpoint,
         delete_student_endpoint
 ):
-    create_student = CreateStudent()
-    create_student.create_student(payload=payload)
+    create_student_endpoint.create_student(payload=payload)
 
-    student_id = create_student.json['id']
-    student_name = create_student.json['name']
-    student_data = create_student.json['data']['student']
-    teacher_data = create_student.json['data']['teacher']
+    student_id = create_student_endpoint.json['id']
+    student_name = create_student_endpoint.json['name']
+    student_data = create_student_endpoint.json['data']['student']
+    teacher_data = create_student_endpoint.json['data']['teacher']
 
-    create_student.create_status_code_is_200()
-    create_student.check_studentID_is_created(student_id)
+    create_student_endpoint.create_status_code_is_200()
+    create_student_endpoint.check_studentID_is_created(student_id)
 
     change_student = ChangeStudent()
     change_student.check_student_name(student_id, student_name)
     change_student.check_student_data(student_id, student_data)
     change_student.check_teacher_data(student_id, teacher_data)
 
-    delete_student = DeleteStudent()
-    delete_student.delete_student(student_id)
+    delete_student_endpoint.delete_student(student_id)
 
 
 @pytest.mark.negative
@@ -140,16 +136,13 @@ def test_add_student_with_negative_data(
         before_after_testing,
         payload,
         create_student_endpoint,
-        read_student_endpoint,
         delete_student_endpoint
 ):
-    create_student = CreateStudent()
-    create_student.create_student(payload=payload)
+    create_student_endpoint.create_student(payload=payload)
 
-    student_id = create_student.json['id']
+    student_id = create_student_endpoint.json['id']
 
-    create_student.create_status_code_is_200()
-    create_student.check_studentID_is_created(student_id)
+    create_student_endpoint.create_status_code_is_200()
+    create_student_endpoint.check_studentID_is_created(student_id)
 
-    delete_student = DeleteStudent()
-    delete_student.delete_student(student_id)
+    delete_student_endpoint.delete_student(student_id)
