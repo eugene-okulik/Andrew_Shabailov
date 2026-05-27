67511cedee2260b5142d9d284c92d3d2253d5dfc
AndrewShabailov <andrewshabailov@gmail.com>
2026-05-27 12:57:47 +0300

    lint fixes

--- homework/andrew_shabailov/lesson23/locustfile_2.py
+++ homework/andrew_shabailov/lesson23/locustfile_2.py
@@ -5,11 +5,9 @@ class Student(HttpUser):
     student_id = 1
     wait_time = between(1, 2)
 
-
     @task
     def get_all_students(self):
         self.client.get("/")
-
 
     @task
     def get_student_by_id(self):
