327cb05b64cc8d17e8378a3d27a0c859c2ae89b4
AndrewShabailov <andrewshabailov@gmail.com>
2026-06-14 17:14:01 +0300

    task1 wrote to test

--- homework/andrew_shabailov/lesson25/task1.py
+++ homework/andrew_shabailov/lesson25/task1.py
@@ -1,20 +1,20 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 
 
-options = Options()
+@pytest.fixture
+def driver():
+    chrome_driver = webdriver.Chrome()
+    chrome_driver.maximize_window()
+    yield chrome_driver
 
-browser = webdriver.Chrome(options=options)
-browser.maximize_window()
-browser.get('https://www.qa-practice.com/elements/input/simple')
 
-search = browser.find_element(By.NAME, 'text_string')
-search.send_keys('Python' + Keys.ENTER)
-
-user_input = browser.find_element(By.CLASS_NAME, 'result-text').text
-
-assert user_input == 'Python'
-
-print(user_input)
+def test_user_input(driver):
+    driver.get('https://www.qa-practice.com/elements/input/simple')
+    search = driver.find_element(By.NAME, 'text_string')
+    search.send_keys('Python' + Keys.ENTER)
+    user_input = driver.find_element(By.CLASS_NAME, 'result-text').text
+    assert user_input == 'Python'
+    print(user_input)
