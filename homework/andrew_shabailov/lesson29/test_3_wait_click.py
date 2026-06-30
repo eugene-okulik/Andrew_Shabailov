7c73fac2db049aab33b18c08fabc2e755042815c
AndrewShabailov <andrewshabailov@gmail.com>
2026-06-30 16:12:07 +0300

    linter fix

--- homework/andrew_shabailov/lesson29/test_3_wait_click.py
+++ homework/andrew_shabailov/lesson29/test_3_wait_click.py
@@ -4,6 +4,6 @@ import re
 
 def test_color_with_timeout(page: Page):
     page.goto('https://demoqa.com/dynamic-properties')
-    color_button  = page.locator('#colorChange')
+    color_button = page.locator('#colorChange')
 
     expect(color_button).to_have_class(re.compile(r'.*text-danger.*'), timeout=10000)
