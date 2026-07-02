b4c643307154d817cb9f8dabda3864e979420f93
AndrewShabailov <andrewshabailov@gmail.com>
2026-07-02 10:37:39 +0300

    added expected result, added click on button, renamed test

--- homework/andrew_shabailov/lesson29/test_3_wait_click.py
+++ homework/andrew_shabailov/lesson29/test_3_wait_click.py
@@ -2,8 +2,11 @@ from playwright.sync_api import Page, expect
 import re
 
 
-def test_color_with_timeout(page: Page):
+def test_clickable_button_after_timeout(page: Page):
     page.goto('https://demoqa.com/dynamic-properties')
     color_button = page.locator('#colorChange')
 
     expect(color_button).to_have_class(re.compile(r'.*text-danger.*'), timeout=10000)
+    expect(color_button, f'"Color Change" button remains inactive').to_be_enabled()
+
+    color_button.click()
